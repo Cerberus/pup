eff575ccab4277ced0ac89afae707ab73027281e
Cerberus <shadow_sun0708@hotmail.com>
2019-02-03 16:22:07 +0700

    update step name

--- src/proxy/index.ts
+++ src/proxy/index.ts
@@ -1,9 +1,23 @@
 import { step } from 'prescript'
 
+const toSentenceCase = (camel: string) => {
+	const spaceCase = camel.replace(/([A-Z])/g, ' $1')
+	const lowerCase = spaceCase
+		.split(' ')
+		.map(word => word.toLowerCase())
+		.join(' ')
+	return lowerCase[0].toUpperCase() + lowerCase.slice(1)
+}
+
 const handler = {
 	get(obj: any, key: any) {
-		return (...options: any[]) => {
-			step(key, () => obj[key](...options))
+		return (...options: unknown[]) => {
+			let stepName = toSentenceCase(key)
+			const optNames = options.filter(opt => typeof opt === 'string')
+			if (optNames.length > 0) {
+				stepName += ` ${optNames.join(' ')}`
+			}
+			step(stepName, () => obj[key](...options))
 			return new Proxy(obj, handler)
 		}
 	},

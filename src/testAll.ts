a121b5f35f3717a3493335536866184600018c06
Cerberus <shadow_sun0708@hotmail.com>
2019-02-11 01:26:58 +0700

    initial testAll

---
+++ src/testAll.ts
@@ -0,0 +1,34 @@
+import * as glob from 'glob'
+import * as chalk from 'chalk'
+import * as childProcess from 'child_process'
+
+const {
+	bgGreen: { bold: green },
+	bgRed: { bold: red },
+} = chalk.default
+
+const files = glob.sync('dist/__tests__/*.js')
+
+Promise.all(
+	files.map(
+		file =>
+			new Promise(resolve => {
+				childProcess.execFile(
+					'./node_modules/.bin/prescript',
+					[file],
+					(e, stdout) => {
+						e
+							? console.log(red(' NG '), file, '\n', stdout)
+							: console.log(green(' OK '), file)
+						resolve(e)
+					},
+				)
+			}),
+	),
+).then(results => {
+	const errorLength = results.filter(result => !!result).length
+	if (errorLength) {
+		console.error('test fail', errorLength, 'case(s)')
+		process.exit(1)
+	}
+})

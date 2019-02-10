83b63925b199d0863f6bc6ed292c4d59a92b6637
Cerberus <shadow_sun0708@hotmail.com>
2019-02-11 02:54:37 +0700

    test all support limitation

--- src/testAll.ts
+++ src/testAll.ts
@@ -8,22 +8,25 @@ const {
 } = chalk.default
 
 const files = glob.sync('dist/__tests__/*.js')
+const limit = require('p-limit')(3)
 
 Promise.all(
-	files.map(
-		file =>
-			new Promise(resolve => {
-				childProcess.execFile(
-					'./node_modules/.bin/prescript',
-					[file],
-					(e, stdout) => {
-						e
-							? console.log(red(' NG '), file, '\n', stdout)
-							: console.log(green(' OK '), file)
-						resolve(e)
-					},
-				)
-			}),
+	files.map(file =>
+		limit(
+			() =>
+				new Promise(resolve => {
+					childProcess.execFile(
+						'./node_modules/.bin/prescript',
+						[file],
+						(e, stdout) => {
+							e
+								? console.log(red(' NG '), file, '\n', stdout)
+								: console.log(green(' OK '), file)
+							resolve(e)
+						},
+					)
+				}),
+		),
 	),
 ).then(results => {
 	const errorLength = results.filter(result => !!result).length

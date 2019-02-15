b5c7d5de6138bd3cfe0ae939581d0eb467f47fb0
Cerberus <shadow_sun0708@hotmail.com>
2019-02-16 00:58:48 +0700

    fix color syntax

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,7 +5,7 @@ import { defer } from 'prescript'
 import { proxify, action, page } from '../proxy'
 
 export const app = proxify({
-	createPage(options: puppeteer.ChromeArgOptions) {
+	createPage: (options: puppeteer.ChromeArgOptions) => {
 		action(async state => {
 			const browser = await puppeteer.launch(options)
 			state.browser = browser
@@ -13,18 +13,18 @@ export const app = proxify({
 			state.page = page
 		})
 	},
-	init(options?: puppeteer.ChromeArgOptions) {
+	init: (options?: puppeteer.ChromeArgOptions) => {
 		app.createPage(options)
 		defer('Close browser', async ({ browser }) => {
 			await browser.close()
 		})
 	},
-	goto(url: string) {
+	goto: (url: string) => {
 		action(async () => {
 			await page.goto(url)
 		})
 	},
-	screenshot(options?: puppeteer.ScreenshotOptions) {
+	screenshot: (options?: puppeteer.ScreenshotOptions) => {
 		action(async () => {
 			const fileName = new Date(Date.now()).toString().slice(16, 24)
 			await page.screenshot({
@@ -33,17 +33,17 @@ export const app = proxify({
 			})
 		})
 	},
-	type(selector: string, text: string, options?: { delay: number }) {
+	type: (selector: string, text: string, options?: { delay: number }) => {
 		action(async () => {
 			await page.type(selector, text, options)
 		})
 	},
-	enter() {
+	enter: () => {
 		action(async () => {
 			await page.keyboard.press('Enter')
 		})
 	},
-	search(selector: string, text: string, options?: { delay: number }) {
+	search: (selector: string, text: string, options?: { delay: number }) => {
 		app.type(selector, text, options).enter()
 	},
 })

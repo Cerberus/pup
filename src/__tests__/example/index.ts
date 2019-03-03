97d7ad5aeccf8c5c7c27e3546d7e1d44a5e09712
Cerberus <shadow_sun0708@hotmail.com>
2019-03-03 16:21:47 +0700

    revert to single selector

--- src/__tests__/example/index.ts
+++ src/__tests__/example/index.ts
@@ -2,14 +2,14 @@ import { app } from 'app/app'
 import { expect } from 'app/assert'
 
 const GOOGLE_SEARCH_BOX = 'input[name=q]'
-const SEARCH_RESULT = '#search h3'
-const SUB_LINK = 'a'
+const FIRST_SEARCH_RESULT = '#rso > *:nth-child(1) h3'
+const SECOND_SEARCH_RESULT = '#rso > *:nth-child(2) h3'
 
 app
 	.init()
 	.goto('https://www.google.com')
 	.search(GOOGLE_SEARCH_BOX, 'google')
 
-expect(SEARCH_RESULT).equal('Google')
-expect(SEARCH_RESULT, 1, SUB_LINK).equal('Google')
+expect(FIRST_SEARCH_RESULT).equal('Google')
+expect(SECOND_SEARCH_RESULT).contain('Google')
 expect(GOOGLE_SEARCH_BOX).valueEqual('google')
--- src/app/assert/index.ts
+++ src/app/assert/index.ts
@@ -4,87 +4,66 @@ import { action, page } from 'proxy'
 import { equal } from 'assert'
 import { step } from 'prescript'
 
-type Selector = string | number
-type Query = { selector: string; index: number | null }
-
-const getName = (queries: Query[]) =>
-	queries
-		.map(({ selector, index }) => `${selector}${index ? `:${index}` : ''}`)
-		.join(' → ')
-
-const notEmpty = <T>(value: T | null | undefined): value is T =>
-	value !== null && value !== undefined
-
-const getQueries = (...args: Selector[]): Query[] => {
-	return args
-		.map((arg, index) => {
-			const nextArg = args[index + 1]
-			return typeof arg === 'string'
-				? {
-						selector: arg,
-						index: typeof nextArg === 'number' ? nextArg : null,
-				  }
-				: undefined
-		})
-		.filter(notEmpty)
+type Comparison = {
+	cb: (received: any, expected: any) => void
+	operation: string
 }
 
-const castToElement = (element: any) => <puppeteer.ElementHandle>(<any>element)
+class Expect {
+	private selector: string
+	private expectedStr: string = ''
 
-const genDebuggingQuery = (queries: Query[]) => {
-	process.env.DEBUG &&
-		console.log(
-			`document${queries.reduce(
-				(acc, { index, selector }) =>
-					acc.concat(
-						`.querySelector${
-							index ? `All('${selector}')[${index}]` : `('${selector}')`
-						}`,
-					),
-				'',
-			)}`,
-		)
-}
+	constructor(selector: string) {
+		this.selector = selector
+	}
 
-export const expect = (...args: Selector[]) => ({
-	expectedStr: '',
-	setExpectedStr(expectedStr: string) {
-		this.expectedStr = expectedStr
-	},
-	setReceiveStr(property: string, comparison: Function) {
-		const queries = getQueries(...args)
+	private setStep(property: string, { cb, operation }: Comparison) {
 		step(
-			`Expect '${getName(queries)}' ${property} = ${this.expectedStr}`,
+			`Expect '${this.selector}' ${property} ${operation} ${this.expectedStr}`,
 			() => {
-				genDebuggingQuery(queries)
 				action(async () => {
-					await page.waitForSelector(queries[0].selector, {
+					const element = await page.waitForSelector(this.selector, {
 						timeout: 5000,
 					})
-					const pagePromise = Promise.resolve(castToElement(page))
-					const element = await queries.reduce(
-						async (elementPromise, { index, selector }) => {
-							const element = await elementPromise
-							return index
-								? (await element.$$(selector))[index]
-								: castToElement(element.$(selector))
-						},
-						pagePromise,
-					)
 					const receivedStr = await element
 						.getProperty(property)
 						.then(obj => obj.jsonValue())
-					comparison(receivedStr, this.expectedStr)
+					cb(receivedStr, this.expectedStr)
 				})
 			},
 		)
-	},
+	}
+
+	private setExpectedStr(expectedStr: string) {
+		this.expectedStr = expectedStr
+	}
+
+	private equalIn(property: string) {
+		this.setStep(property, { cb: equal, operation: '=' })
+	}
+
+	private containIn(property: string) {
+		this.setStep(property, {
+			cb: (receivedStr: string, expectedStr: string) =>
+				receivedStr.indexOf(expectedStr) !== -1,
+			operation: 'contains',
+		})
+	}
+
 	equal(expectedStr: string) {
 		this.setExpectedStr(expectedStr)
-		this.setReceiveStr('innerHTML', equal)
-	},
+		this.equalIn('innerHTML')
+	}
+
 	valueEqual(expectedStr: string) {
 		this.setExpectedStr(expectedStr)
-		this.setReceiveStr('value', equal)
-	},
-})
+		this.equalIn('value')
+	}
+
+	contain(expectedStr: string) {
+		this.setExpectedStr(expectedStr)
+		this.containIn('innerHTML')
+	}
+}
+
+export const expect = (selector: string) => new Expect(selector)

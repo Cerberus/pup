8582d6a72b86b44a9537cda17088b6e275265a74
Cerberus <shadow_sun0708@hotmail.com>
2019-03-25 23:20:50 +0700

    make an example shortly

--- src/__tests__/example/index.ts
+++ src/__tests__/example/index.ts
@@ -3,7 +3,6 @@ import { expect } from 'modules/assert'
 
 const GOOGLE_SEARCH_BOX = 'input[name=q]'
 const FIRST_SEARCH_RESULT = '#rso > *:nth-child(1) h3'
-const SECOND_SEARCH_RESULT = '#rso > *:nth-child(2) h3'
 const GOOGLE_LINK = "//*[@id='rso']//*[text()='Google']"
 
 app
@@ -12,8 +11,6 @@ app
 	.search(GOOGLE_SEARCH_BOX, 'google')
 
 expect(FIRST_SEARCH_RESULT).equal('Google')
-expect(SECOND_SEARCH_RESULT).contain('Google')
-expect(GOOGLE_SEARCH_BOX, 'value').equal('google')
 
 app.clickText(GOOGLE_LINK)
 
